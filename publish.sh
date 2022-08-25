03d3142314c16873f1606bcc89a4443eaf04ea21
yangleilei <yangleilei@pwrd.com>
2022-08-25 14:14:41 +0800

    🐎 ci(publish ): 完善

--- publish.sh
+++ publish.sh
@@ -1,8 +1,41 @@
-# !/bin/bash
-echo 'begin commit'
-git commit -a
-# 保证是npmjs，不能使用国内镜像
-npm version patch
-npm publish
-git push tag
-git push branch
+#!/usr/bin/env sh
+
+# 发生任何错误时终止
+set -e
+
+# 选择版本号
+VERSION=$(select-version-cli)
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo # (optional) 空行
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+    npm version $VERSION --message "[release] $VERSION"
+
+    # changelog
+    yarn changelog
+    # commmit
+    echo '====== begin commit ========='
+    git add -A
+    git commit -m "[build] $VERSION"
+
+    # publish
+    echo '======== begin publish ======='
+    echo $VERSION
+    npm publish
+
+    # push
+    echo '===== begin push ======'
+    git push origin develop
+    git checkout master
+    git rebase develop
+    git push origin master
+    git checkout develop
+    # push tag
+    git push origin v$VERSION
+    echo '====== push end ======'
+
+else
+    echo '请选择版本号'
+
+fi
